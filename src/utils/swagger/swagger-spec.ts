6649617bffe77869d5aa2a1f58cac3f80849c9f8
Bittu Kumar <64475463+BittuKumar183040@users.noreply.github.com>
2025-10-12 05:12:30 +0530

    removed server dropdown

--- src/utils/swagger/swagger-spec.ts
+++ src/utils/swagger/swagger-spec.ts
@@ -227,7 +227,6 @@ const paths: OpenAPIV3.PathsObject = {
 export const swaggerSpec: OpenAPIV3.Document = {
   openapi: "3.0.0",
   info: { title: "Credential Issuance API", version: "1.0.0" },
-  servers: [{ url: "http://localhost:3000" }],
   paths,
   components: { schemas },
 };
